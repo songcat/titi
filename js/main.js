f26c9e3823c064ca1c7a372f6ae37a4e93951dde
Annie <a>
2014-03-30 03:48:57 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
     })
     $(".rslides").responsiveSlides();
     $(".slider").css('height',$(".rslides").height());
+    $(window).resize(function(){
+        $(".slider").css('height',$(".rslides").height());
+    })
+
+
+    $("#btn-nav").click(function(e){
+        e.preventDefault();
+        $(this).toggleClass("is-open");
+        $('header nav > ul').stop().slideToggle("600");
+        if(linkNavH == null) linkNavH = $('nav .has-sub-nav').height();
+        $(".btn-sub-nav").css("height", linkNavH);
+    });
+
 });
